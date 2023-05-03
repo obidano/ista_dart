4527547ae5979c199018627c7c6cd6d9d84da77a
obidano <omotetedan2@gmail.com>
2023-05-03 12:27:40 +0100

    Create 01.bases.dart

---
+++ 01.bases.dart
@@ -0,0 +1,12 @@
+
+void main(){
+  print("Hello ISTA");
+  int montant=500;
+  double tauxCommission=0.20;
+  double commission= montant * tauxCommission;
+  print("Commission: $commission USD");
+  double montantAPayer= montant - commission;
+  print("Montant à payer: $montantAPayer USD");
+  
+  
+}
